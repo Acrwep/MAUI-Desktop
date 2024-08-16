03d53d7347e0bb5febf91bcdca090f4dcd7822d8
balaji_hublog <hublogfrontend@gmail.com>
2024-08-16 14:34:34 +0530

    login page ui chnages

--- Hublog.Desktop/MauiProgram.cs
+++ Hublog.Desktop/MauiProgram.cs
@@ -5,7 +5,7 @@ namespace Hublog.Desktop
 {
     public static class MauiProgram
     {
-        public static string OnlineURL = "https://localhost:7263/";
+        public static string OnlineURL = "https://localhost:44322/";
 
         public static Users Loginlist = new Users();
         public static string token = "";
