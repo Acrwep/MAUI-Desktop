11d8bc2c5257f98d2228ed0135f5cd04e0e3f990
balaji_hublog <hublogfrontend@gmail.com>
2024-08-19 18:24:28 +0530

    ui changes

--- Hublog.Desktop/Platforms/Android/MainApplication.cs
+++ Hublog.Desktop/Platforms/Android/MainApplication.cs
@@ -6,6 +6,7 @@ namespace Hublog.Desktop
     [Application]
     public class MainApplication : MauiApplication
     {
+        public static string OnlineURL = "https://localhost:44322/";
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
