258b7abfcb53bcc0705a089ce86d71ccaa5c4e3e
sethu_hublog <actewpdeveloper@gmail.com>
2024-08-24 13:01:22 +0530

    dashboard.js created

---
+++ Hublog.Desktop/wwwroot/js/dashboard.js
@@ -0,0 +1,35 @@
+﻿function openBreakTimerModal() {
+    console.log("openBreakModal called");
+    const modalElement = new bootstrap.Modal(document.getElementById('breakTimerModal'), {
+        backdrop: 'static',
+        keyboard: true
+    });
+    modalElement.show();
+}
+
+function openBreakModal() {
+    console.log("openBreakModal called");
+    const modalElement = new bootstrap.Modal(document.getElementById('breakModal'), {
+        backdrop: 'static',
+        keyboard: true
+    });
+    modalElement.show();
+}
+
+function closeBreakModal() {
+    var modal = bootstrap.Modal.getInstance(document.getElementById('breakModal'));
+    modal.hide();
+}
+
+function closeBreakTimerModal() {
+    var modal = bootstrap.Modal.getInstance(document.getElementById('breakTimerModal'));
+    modal.hide();
+}
+
+function changeResumeButtonColorToRed() {
+    var button = document.querySelector('.breakResume_button');
+    if (button) {
+        button.style.backgroundColor = 'red';
+        button.style.color = 'white';
+    }
+}
